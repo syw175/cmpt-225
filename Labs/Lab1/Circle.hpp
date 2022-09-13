4a13085db7bff2d5a1cfe7e702d19f06f851c32d
Steven Wong <syw14@sfu.ca>
2022-09-12 23:52:42 -0700

    implement Circle class header file

--- Labs/Lab1/Circle.hpp
+++ Labs/Lab1/Circle.hpp
@@ -6,3 +6,51 @@
 * Author: Steven Wong
 * Creation date: September 12, 2022
 */
+
+
+#include <iostream>
+#include <stdio>
+
+using namespace std;
+
+class Circle {
+  private:
+	int xCoordinate;
+	int yCoordinate;
+	double radius;
+
+  public:
+	// Default constructor
+	Circle();
+
+	// Parameterized constructor
+	Circle(int x, int y, double radius);
+
+	// Default destructor
+	~Circle();
+
+	// Return the circle's centre x-coordinate
+	int getX();
+
+	// Return the circle's centre y-coordinate
+	int getY();
+
+	// Return the circle's radius
+	double getRadius();
+
+	// Move the circle the given horizontal and vertical distances
+	void move(int horiz, int vert);
+
+	// Change the circle's radius to r, or to 10.0 if r is invalid
+	void setRadius(double r);
+
+	// Compute and returns the circle's area
+	double computeArea();
+
+	// Display the circle's data members like this: x = 0, y = 11, radius = 0.2
+	void displayCircle();
+
+	// Returns true if c intersects the calling circle
+	bool intersect(Circle c);
+};
+
