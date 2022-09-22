6e35d26331e1be0461a7ffa4b5388f98ba7ee0ac
Steven Wong <syw14@sfu.ca>
2022-09-22 16:29:07 -0700

    fix program crash when memory allocation fails

--- Labs/Lab2/sum-array/sum_array.cpp
+++ Labs/Lab2/sum-array/sum_array.cpp
@@ -17,7 +17,8 @@ int sumArray(int arr[], unsigned int arrSize);
 int main(void)
 {
     int * arr = NULL;        
-    unsigned int arrSize = 0;     
+    unsigned int arrSize = 0;    
+    int sumTotal = 0; 
 
     // Get the array size from the user using cin:
     cout << "Enter the number of values to be summed: ";
@@ -35,17 +36,19 @@ int main(void)
             cout << "Enter the integer value for array index " << i << ": ";
             cin >> arr[i];
         }
+        // Print the sum of the values.
+        sumTotal = sumArray(arr, arrSize);
+        cout << "The sum of the values in the array is: " << sumTotal << endl;
+
+        // Careful here! Make sure you know the difference between the delete[ ] operator and the delete operator.
+        // Delete the dynamically allocated array before terminating the program.
+        delete [] arr;
+
+        // Return the sum of the values
+        return sumTotal;
     }
-
-    // Print the sum of the values.
-    int sumTotal = sumArray(arr, arrSize);
-    cout << "The sum of the values in the array is: " << sumTotal << endl;
-
-    // Careful here! Make sure you know the difference between the delete[ ] operator and the delete operator.
-    // Delete the dynamically allocated array before terminating the program.
-    delete [] arr;
-
-    // Return the sum of the values
+    // Give error message
+    cout << "Error: Failed to allocate memory..." << endl;
     return sumTotal;
 }
 
