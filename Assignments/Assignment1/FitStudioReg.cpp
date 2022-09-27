2a4077bc2a8d88d922d98523bbd6f59053e802da
Steven Wong <syw14@sfu.ca>
2022-09-27 16:13:11 -0700

    add prompts to user

--- Assignments/Assignment1/FitStudioReg.cpp
+++ Assignments/Assignment1/FitStudioReg.cpp
@@ -16,7 +16,7 @@
 using std::cout;
 
 // Function prototypes
-void add(List *members);
+void add(List &members);
 void remove(List *members);
 void search(List *members);
 void modify(List *members);
@@ -46,7 +46,7 @@ int main() {
         cout << endl;
         input = tolower(input);
 		switch(input) {
-            case 'a': add(Members); break;
+            case 'a': add(*Members); break;
             case 'r': remove(Members); break;
             case 's': search(Members); break;
             case 'm': modify(Members); break;
@@ -59,14 +59,50 @@ int main() {
 }
 
 // TODO: Implement methods
-void add(List *members)
+void add(List &members)
 {
+    // Initialize variables
+    string name, phone, email, creditCard;
+
+    // Prompt user
+    cout << "You have chosen to add a new member" << endl; 
+    cout << "Enter the full name of the new member: "; 
+    cin >> name; 
+
+    cout << "Enter the phone number of the new member in the format 'XXX-XXX-XXXX': ";
+    cin >> phone; 
+
+    cout << "Enter the email of the new member: ";
+    cin >> email;
+
+    cout << "Enter the credit card number of the new member: ";
+    cin >> creditCard; 
+
+    // Create a new member with the given information
+    Member *newMember = new Member(name, phone, email, creditCard);
+
+    if (!members.insert(*newMember))
+    {
+        cout << "Adding new member failed!" << endl;
+        delete newMember;
+    }
+    else 
+    {
+        cout << "Success!" << endl;
+    }
+}
+
+
+// Remove a member from the list
+void remove(List &members)
+{
+    // Ask user for member's phone number
+  
+
+    // Search for the member in the list
 
 }
-void remove(List *members)
-{
 
-}
 void search(List *members)
 {
 
@@ -75,7 +111,9 @@ void modify(List *members)
 {
 
 }
+// Print all members in the list
 void print(List *members)
 {
-
+    cout << "Now printing the list containing " << members->getElementCount() << " elements" << endl;
+    members->printList();
 }
