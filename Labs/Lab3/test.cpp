f3e5799a01f10e140c19cacd4522469d48ed7f1e
Steven Wong <syw14@sfu.ca>
2022-09-29 18:36:20 -0700

    minor changes

--- Labs/Lab3/test.cpp
+++ Labs/Lab3/test.cpp
@@ -8,10 +8,52 @@
  * Author:
  */
 
+#include <iostream>
+#include "List.h"
 
+using namespace std;
+
+void listTest();
 int main () {
 
-  
+  listTest();
    
   return 0;
+}
+
+void listTest(){
+
+   List * ls1 = new List();
+    
+   /* Test Case */
+   cout << "Appending 1,2,3 to the first List object." << endl;
+   ls1->append(1);
+   ls1->append(2);
+   ls1->append(3);
+   cout << endl << "Printing the first List object." << endl;
+   ls1->printList();
+   cout << endl << "Does it contain {1,2,3}?" << endl;
+	
+   cout << endl << "Make a copy of the List object." << endl;
+   List * ls2 = new List(*ls1);
+   cout << endl << "Print second List object." << endl;
+   ls2->printList();
+   cout << endl << "Does it contain {1,2,3}?" << endl;
+	
+   cout << endl << "Append 4,5,6 to the first List object." << endl;
+   ls1->append(4);
+   ls1->append(5);
+   ls1->append(6);
+   cout << endl << "Print first List object." << endl;
+   ls1->printList();
+   cout << endl << "Does it contain {1,2,3,4,5,6}?" << endl;
+	
+   cout << endl << "Print second List object: (what does it contain?)." << endl;
+   ls2->printList();
+	
+   cout << endl << "If it was shallow copied, it should contain: {1,2,3,4,5,6}.";
+   cout << endl << "If it was deep copied, it should contain: {1,2,3}." << endl;	
+   cout << endl;
+	
+   return;
 }
