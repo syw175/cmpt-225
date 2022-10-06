5428c1818f4875778e0724d4bb877617f446691b
Steven Wong <syw14@sfu.ca>
2022-10-05 17:00:03 -0700

    some work done on q3

--- Assignments/Assignment2/Q3/Eval.cpp
+++ Assignments/Assignment2/Q3/Eval.cpp
@@ -13,6 +13,44 @@
 
 using namespace std;
 
+// Remove after completing
+// while T is not EOF or the operator stack is non empty
+
+//     if T is a number:
+//         push T to the number stack; get the next token
+		
+//     else if T is a left parenthesis:
+//         push T to the operator stack; get the next token
+		
+//     else if T is a right parenthesis:
+//         if the top of the operator stack is a left parenthesis:
+//             pop it from the operator stack; get the next token:
+//         else:
+//             pop the top two numbers and the top operator
+//             perform the operation
+//             push the result to the number stack
+	
+//     else if T is +, - or EOF:
+//         if the operator stack is nonempty and the top is one 
+//                                             of +, -, *, /:
+//             pop the top two numbers and the top operator
+//             perform the operation
+//             push the result to the number stack
+//         else:
+//             push T to the operator stack; get the next token
+
+//     else if T is * or /:
+//         if the operator stack is nonempty and the top is one of *, /:
+//             pop the top two numbers and the top operator
+//             perform the operation
+//             push the result to the number stack
+//         else:
+//             push T to the operator stack; get the next token
+
+
+
+// For testing purposes, function prototypes
+int evalauteOperation(Token &operand1, Token &operand2, Token &operation);
 
 
 int main () {
@@ -20,6 +58,69 @@ int main () {
     Token t;
 
     Stack<Token> numstack, opstack;  // 2x Stacks of type Token
+
+
+
+    // //  Token  //
+// class Token {
+//     public:
+//         TokenType tt;
+//         string text;
+//         int val;
+// };
+
+
+    // WIP 
+    t = S.getnext();
+
+    // while T is not EOF or the operator stack is non empty
+    while (t.tt != eof || !opstack.isEmpty())
+    {
+        // if T is a number:
+        // push T to the number stack; get the next token
+        if (t.tt == integer)
+        {
+            numstack.push(t);
+            t = S.getnext();
+        }
+        // else if T is a left parenthesis:
+        // push T to the operator stack; get the next token
+        else if (t.tt == lptok)
+        {
+            opstack.push(t);
+            t = S.getnext();
+        }
+        // else if T is a right parenthesis:
+        else if (t.tt == rptok)
+        {
+            // if the top of the operator stack is a left parenthesis:
+            if (opstack.peek().tt == lptok)
+            {
+                // pop it from the operator stack; get the next token:
+                // Check this
+                opstack.pop();
+                t = S.getnext();
+            }
+            else
+            {
+                // pop the top two numbers and the top operator
+                Token operand1 = numstack.pop(); 
+                Token operand2 = numstack.pop();
+                Token operation = opstack.pop();
+
+                // perform the operation
+                Token result = 
+
+                // push the result to the number stack
+                
+            }
+        }        
+
+
+
+        
+
+    }
     
 
     t = S.getnext();
@@ -42,3 +143,29 @@ int main () {
     return 0;
 }
 
+// Assumes that all tokens have valid values
+int evalauteOperation(Token &operand1, Token &operand2, Token &operation)
+{
+
+    // typedef enum { pltok, mitok, asttok, slashtok, lptok, rptok, integer, errtok, eof }
+
+    TokenType symbol = operation.tt;
+    
+    switch (symbol)
+    {
+        case 0:
+            return operand1.val + operand2.val;
+
+        case 1:
+            return operand1.val - operand2.val;
+
+        case 2:
+            return operand1.val * operand2.val;
+
+        case 3:
+            return operand1.val / operand2.val;
+        
+        default:
+            return 0;
+    }
+}
