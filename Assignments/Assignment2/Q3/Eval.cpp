6e9f9e475e9e70588a00f4ebce41475cc09ea53f
Steven Wong <syw14@sfu.ca>
2022-10-08 13:17:20 -0700

    remove comments and misc clean up

--- Assignments/Assignment2/Q3/Eval.cpp
+++ Assignments/Assignment2/Q3/Eval.cpp
@@ -13,43 +13,8 @@
 
 using namespace std;
 
-// Remove after completing
-// while T is not EOF or the operator stack is non empty
-
-//     if T is a number:
-//         push T to the number stack; get the next token
-		
-//     else if T is a left parenthesis:
-//         push T to the operator stack; get the next token
-		
-//     else if T is a right parenthesis:
-//         if the top of the operator stack is a left parenthesis:
-//             pop it from the operator stack; get the next token:
-//         else:
-//             pop the top two numbers and the top operator
-//             perform the operation
-//             push the result to the number stack
-	
-//     else if T is +, - or EOF:
-//         if the operator stack is nonempty and the top is one 
-//                                             of +, -, *, /:
-//             pop the top two numbers and the top operator
-//             perform the operation
-//             push the result to the number stack
-//         else:
-//             push T to the operator stack; get the next token
-
-//     else if T is * or /:
-//         if the operator stack is nonempty and the top is one of *, /:
-//             pop the top two numbers and the top operator
-//             perform the operation
-//             push the result to the number stack
-//         else:
-//             push T to the operator stack; get the next token
-
 // For testing purposes, function prototypes
 Token &evalauteOperation(Stack<Token> &numstack, Stack<Token> &opstack);
-// Token &evalauteOperation(Token &operand1, Token &operand2, Token &operation);
 
 
 int main () {
@@ -63,36 +28,31 @@ int main () {
     // while T is not EOF or the operator stack is non empty
     while (t.tt != eof || !opstack.isEmpty()) 
     {
-        // if T is a number:
-        // push T to the number stack; get the next token
+        // if T is a number: push T to the number stack; get the next token
         if (t.tt == integer)
         {
             numstack.push(t);
             t = S.getnext();
         }
-        // else if T is a left parenthesis:
-        // push T to the operator stack; get the next token
+        // else if T is a left parenthesis: push T to the operator stack; get the next token
         else if (t.tt == lptok)
         {
             opstack.push(t);
             t = S.getnext();
         }
-        // else if T is a right parenthesis:
+        // else if T is a right parenthesis....
         else if (t.tt == rptok)
         {
             // if the top of the operator stack is a left parenthesis:
             if (opstack.peek().tt == lptok)
             {
                 // pop it from the operator stack; get the next token:
-                // Check this
                 opstack.pop();
                 t = S.getnext();
             }
             else
             {
-                // pop the top two numbers and the top operator
-                // perform the operation
-                // push the result to the number stack   
+                // pop the top two numbers and the top operator, perform the operation, and push the result to the number stack
                 numstack.push(evalauteOperation(numstack, opstack));
             }
         }        
@@ -102,9 +62,7 @@ int main () {
             // if the operator stack is nonempty and the top is one of +, -, *, /:
             if (!opstack.isEmpty() && (opstack.peek().tt == pltok || opstack.peek().tt == mitok || opstack.peek().tt == asttok || opstack.peek().tt == slashtok))
             {
-                // pop the top two numbers and the top operator
-                // perform the operation
-                // push the result to the number stack
+                // pop the top two numbers and the top operator, perform the operation, and push the result to the number stack
                 numstack.push(evalauteOperation(numstack, opstack));
             }
             // push T to the operator stack; get the next token
@@ -120,9 +78,7 @@ int main () {
             // if the operator stack is nonempty and the top is one of *, /:
             if (!opstack.isEmpty() && (opstack.peek().tt == asttok || opstack.peek().tt == slashtok))
             {
-                // pop the top two numbers and the top operator
-                // perform the operation
-                // push the result to the number stack
+                // pop the top two numbers and the top operator, perform the operation, and push the result to the number stack
                 numstack.push(evalauteOperation(numstack, opstack));
             }
             else
@@ -133,12 +89,9 @@ int main () {
             }
         }
     }
-
     cout << numstack.peek().val << endl;
     return 0;
 }
-
-
 
 // Assume client code is right for now
 Token &evalauteOperation(Stack<Token> &numstack, Stack<Token> &opstack)
