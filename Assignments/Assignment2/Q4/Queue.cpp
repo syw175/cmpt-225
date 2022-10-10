9b8460b59fe4e99854f7410fdb3a437e521a12bd
Steven Wong <syw14@sfu.ca>
2022-10-09 19:03:04 -0700

    some work done on q4 queue()

--- Assignments/Assignment2/Q4/Queue.cpp
+++ Assignments/Assignment2/Q4/Queue.cpp
@@ -1,13 +1,19 @@
 /* 
- * Queue.cpp - Does not work well!
+ * Queue.cpp - Does not work well! CHANGE THIS LATER
  *
  * Description: Array-based implementation of Queue as an ADT class
  * Class Invariant: Queue maintained in FIFO order
  *
- * Author:
- * Date:
+ * Author: Steven Wong
+ * Date: October 2022
  */
  
+
+ // What else to change?? 
+ // ElementCount is incorrectly set at some point in this program, go fix it 
+ // Consider trying to handle exceptions... specifically where the preconditions of dequeue() and peek() have not been satisfied
+ // Also need to add a proper test Driver for this program to test all the new updated functionality
+
 #include <iostream>
 #include "Queue.h"
 
@@ -15,35 +21,114 @@ using std::cout;
 using std::endl;
 
 // Description: Constructor
-Queue::Queue() {}
+Queue::Queue()
+{
+    elements = new int[INITIAL_CAPACITY];
+}
+
+// Description: Destructor
+Queue::~Queue()
+{
+    delete[] elements;
+    elements = nullptr;
+}
+
+// ToDO
+// Method to enlarge the array if it becomes full
+// Takes a int for choice FOR NOW
+void Queue::resize(void)
+{
+    if (elementCount == capacity)
+    {
+        // Update capacity size
+        capacity *= 2;
+        // Create a new array of elements
+        int *newElements = new int[capacity];
+
+        // Copy over the previous elements from the back to the front and then delete the queue
+        // Start from the back elements and 
+        // for (unsigned int i =
+    }
+    else
+    {
+        // If choice is not 0, that means the array is too big and we need to shrink it
+
+
+    }
+    return;
+}
 
 // Description: Inserts element x at the back of Queue
 // Time Efficiency: O(1)
-void Queue::enqueue(int x) {
+
+// ToDO
+// Rewrite this function and also change the time efficiency of this enqueue + description
+// Double the capacity of array whenever I enqueue into a full array... expensive resizing becomes amortized across enough operations
+// Feel free to add a private helper method to the queue class
+void Queue::enqueue(int x)
+{
+    // If the array is full we need to resize it
+    resize();
+
+    // Enqueue our new element and update elementCount and back index
     elementCount++;
     elements[backindex] = x;
-    backindex = (backindex + 1) % capacity;    
+    backindex++;
     return;
 } 
 
 // Description: Removes the frontmost element
 // Precondition: Queue not empty
 // Time Efficiency: O(1)
-void Queue::dequeue() {
+
+// Rewrite this function to make this program more efficient, update description + time efficiency
+// Halve the capacity of the queue whenever the array is less than 1/4 full 
+// Invariant: The min capacity cannot drop below the value of INITAL_CAPACITY
+
+// TODO
+void Queue::dequeue()
+{
+    // If trying to dequeue from an empty array... raise an exception
+    if (isEmpty())
+    {
+        cout << "Exception: Cannot dequuee from an empty array" << endl;
+        abort();
+    }
+
     elementCount--;
-    frontindex = (frontindex + 1) % capacity;
+    frontindex++;
+    // frontindex = (frontindex + 1) % capacity;
+
+
+    // Check on the state of the array after dequeueing an element
+    // If it is less than 1/4 full AND if min capacty won't drop below the INITAL_CAPACITY...
+    //               half the capacity of the queue
+    resize();
     return;
 } 
 
 // Description: Returns a copy of the frontmost element
 // Precondition: Queue not empty
 // Time Efficiency: O(1)
-int Queue::peek() const {
+int Queue::peek() const
+{
     return elements[frontindex];    
 } 
 
 // Description: Returns true if and only if Queue empty
 // Time Efficiency: O(1)
-bool Queue::isEmpty() const {
+bool Queue::isEmpty() const
+{
     return elementCount == 0;
+}
+
+// Description: Used to print out the Queue for testing purposes
+std::ostream &operator<<(std::ostream &os, const Queue &q)
+{
+    // Need Correction here
+    for (unsigned int i = 0; i < q.elementCount; i++)
+    {
+        os << q.elements[i];
+    }
+    return os;
 }
--- Assignments/Assignment2/Q4/Queue.h
+++ Assignments/Assignment2/Q4/Queue.h
@@ -4,8 +4,8 @@
  * Description: Array-based implementation of Queue as an ADT class
  * Class Invariant: Queue maintained in FIFO order
  *
- * Author:
- * Date:
+ * Author: Steven Wong
+ * Date: October 2022
  */
  
 #ifndef QUEUE_H
@@ -17,7 +17,7 @@ class Queue {
 	
     private:
         static unsigned int const INITIAL_CAPACITY = 6; // Constant INITIAL_CAPACITY
-        int elements[INITIAL_CAPACITY];                 // To do: replace this by int * elements -> Question 4.a)
+        int *elements;
 
         unsigned int elementCount = 0;                  // Number of elements in the Queue - if you need it!
         unsigned int capacity = INITIAL_CAPACITY;       // Actual capacity of the data structure (number of cells in the array)
@@ -25,6 +25,7 @@ class Queue {
         unsigned int backindex = 0;                     // Index of where the next element will be enqueued                     
 
 /* You can add private methods. */
+        void resize(void);
 
 
     public:
@@ -33,6 +34,9 @@ class Queue {
 
         // Description: Constructor
         Queue();
+
+        // Description: Destructor
+        ~Queue();
 
         // Description: Inserts element x at the back of Queue
         // Time Efficiency: O(1)
@@ -51,7 +55,9 @@ class Queue {
         // Description: Returns true if and only if Queue empty
         // Time Efficiency: O(1)
         bool isEmpty() const;
-        
+
+        // Description: Testing function used to print out the Queue
+        friend std::ostream &operator<<(std::ostream &os, const Queue &q);
 };
 #endif
 
