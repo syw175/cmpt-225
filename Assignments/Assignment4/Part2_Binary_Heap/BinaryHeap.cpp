d58370bf603862f260af207c86eb0773a61990c5
Steven Wong <syw14@sfu.ca>
2022-11-02 13:08:49 -0700

    Assignment4 distribution code

---
+++ Assignments/Assignment4/Part2_Binary_Heap/BinaryHeap.cpp
@@ -0,0 +1,75 @@
+/* 
+ * BinaryHeap.cpp
+ *
+ * Description: ____________ Binary Heap ADT class.
+ *
+ * Class Invariant:  Always a ____________ Binary Heap.
+ * 
+ * Author: 
+ * Last Modification: Oct. 2022
+ *
+ */  
+
+#include <iostream>
+#include "BinaryHeap.h"  // Header file
+
+using std::cout;
+using std::endl;
+  
+
+
+// Description: Removes (but does not return) the necessary element.
+// Precondition: This Binary Heap is not empty.
+// Exceptions: Throws EmptyDataCollectionException if this Binary Heap is empty.
+// Time Efficiency: O(log2 n)
+template <class ElementType>
+void BinaryHeap<ElementType>::remove() {  
+
+   if(elementCount == 0) 
+      throw EmptyDataCollectionException("remove() called with an empty BinaryHeap.");
+
+   elements[0] = elements[elementCount - 1];
+   elementCount--;
+   reHeapDown(0);
+   
+   return;   
+}
+
+// Utility method
+// Description: Recursively put the array back into a ____________ Binary Heap.
+template <class ElementType>
+void BinaryHeap<ElementType>::reHeapDown(unsigned int indexOfRoot) {
+
+   unsigned int indexOfMinChild = indexOfRoot;
+   
+   // Find indices of children.
+   unsigned int indexOfLeftChild = 2 * indexOfRoot + 1;
+   unsigned int indexOfRightChild = 2 * indexOfRoot + 2;
+
+   // Base case: elements[indexOfRoot] is a leaf as it has no children
+   if (indexOfLeftChild > elementCount-1) return;
+
+   // If we need to swap, select the smallest child
+   // If (elements[indexOfRoot] > elements[indexOfLeftChild])
+   if ( ! (elements[indexOfRoot] <= elements[indexOfLeftChild]) )
+      indexOfMinChild = indexOfLeftChild;
+
+   // Check if there is a right child, is it the smallest?
+   if (indexOfRightChild < elementCount) {
+      // if (elements[indexOfMinChild] > elements[indexOfRightChild])
+      if ( ! (elements[indexOfMinChild] <= elements[indexOfRightChild]) )
+         indexOfMinChild = indexOfRightChild;
+   }
+
+   // Swap parent with smallest of children.
+   if (indexOfMinChild != indexOfRoot) {
+      
+      ElementType temp = elements[indexOfRoot];
+      elements[indexOfRoot] = elements[indexOfMinChild];
+      elements[indexOfMinChild] = temp;
+      
+      // Recursively put the array back into a heap
+      reHeapDown(indexOfMinChild);
+   }
+   return;
+} 
