ec2bff4c8b3653382503e6e15286eb0331bd3505
Steven Wong <syw14@sfu.ca>
2022-11-10 12:41:39 -0800

    priorityQueue mostly implmented, some issues w/ destructor need resolving

---
+++ Assignments/Assignment4/Part3_Priority_Queue/PriorityQueue.cpp
@@ -0,0 +1,104 @@
+/*
+ * PriorityQueue.cpp
+ *
+ * Description: Implemented using the Binary Heap Data Collection ADT from Part 2 as the underlying data structure.
+ *              Functionally, the operations of the Priority class are wrapper methods to the public methods of the
+ *              Binary Heap ADT class.
+ * 
+ * Author: Steven Wong
+ * Last Modification: November 2022
+ */
+
+#include "PriorityQueue.h"
+
+// Default Constructor
+template <class ElementType>
+PriorityQueue<ElementType>::PriorityQueue()
+{
+    priorityQueue = new BinaryHeap<ElementType>();
+}
+
+// Copy Constructor
+template <class ElementType>
+PriorityQueue<ElementType>::PriorityQueue(const PriorityQueue<ElementType> &q)
+{
+
+}
+
+// Destructor
+// Why does this cause a compiler error? 
+template <class ElementType>
+PriorityQueue<ElementType>::~PriorityQueue()
+{
+    delete priorityQueue;
+    priorityQueue = nullptr;
+}
+
+// Description: Returns true if this Priority Queue is empty, otherwise false.
+// Postcondition: This Priority Queue is unchanged by this operation.
+// Time Efficiency: O(1)
+template <class ElementType>
+bool PriorityQueue<ElementType>::isEmpty() const
+{
+    return priorityQueue->getElementCount() == 0;
+}
+
+// Description: Inserts newElement in this Priority Queue and 
+//              returns true if successful, otherwise false.
+// Time Efficiency: O(log2 n)
+template <class ElementType>
+bool PriorityQueue<ElementType>::enqueue(ElementType &newElement)
+{
+    return priorityQueue->insert(newElement);
+}
+
+// Description: Removes (but does not return) the element with the next
+//              "highest" priority value from the Priority Queue.
+// Precondition: This Priority Queue is not empty.
+// Exception: Throws EmptyDataCollectionException if Priority Queue is empty.
+// Time Efficiency: O(log2 n)
+template <class ElementType>
+void PriorityQueue<ElementType>::dequeue()
+{
+    try
+    {
+        priorityQueue->remove();
+    }
+    catch(EmptyDataCollectionException &e)
+    {
+        throw EmptyDataCollectionException("Dequeue() method called with an empty Priority Queue");
+    }
+}
+
+// Description: Returns (but does not remove) the element with the next 
+//              "highest" priority from the Priority Queue.
+// Precondition: This Priority Queue is not empty.
+// Postcondition: This Priority Queue is unchanged by this operation.
+// Exception: Throws EmptyDataCollectionException if this Priority Queue is empty.
+// Time Efficiency: O(1)
+template <class ElementType>
+ElementType& PriorityQueue<ElementType>::peek() const
+{
+    try
+    {
+        priorityQueue->retrieve();
+    }
+    catch(EmptyDataCollectionException &e)
+    {
+        throw EmptyDataCollectionException("Peek() method called with an empty Priority Queue");
+    }
+}
+
+// Description: Prints all elements in the priority queue for testing purposes.
+// Time Efficiency: O(n)
+// Precondition: This Priority Queue is not empty.
+// Postcondition: This Priority Queue is unchanged by this operation.
+// Exception: Throws EmptyDataCollectionException if Priority Queue is empty.
+template <class ElementType>
+void PriorityQueue<ElementType>::printPriorityQueue() const
+{
+    if (priorityQueue->getElementCount() == 0)
+        throw EmptyDataCollectionException("PrintPriorityQueue() method called with an empty Priority Queue");
+    else
+        priorityQueue->print();
+}
--- Assignments/Assignment4/Part3_Priority_Queue/PriorityQueue.h
+++ Assignments/Assignment4/Part3_Priority_Queue/PriorityQueue.h
@@ -14,16 +14,57 @@
 
 #include <iostream>
 #include "BinaryHeap.h"
+#include "EmptyDataCollectionException.h"
 
 template <class ElementType>
 class PriorityQueue
 {
     private:
+        // Underlying data structure of our PriorityQueue
+        BinaryHeap<ElementType> priorityQueue;
 
-
+    /******* Start of Priority Queue Public Interface *******/
     public:
-}
+        // Default Constructor
+        PriorityQueue();
 
+        // Copy Constructor
+        PriorityQueue(const PriorityQueue<ElementType> &q)
 
-#endif
+        // Destructor Necessary or no?
+        ~PriorityQueue();
+
+        // Description: Returns true if this Priority Queue is empty, otherwise false.
+        // Postcondition: This Priority Queue is unchanged by this operation.
+        // Time Efficiency: O(1)
+        bool isEmpty() const;
+
+        // Description: Inserts newElement in this Priority Queue and 
+        //              returns true if successful, otherwise false.
+        // Time Efficiency: O(log2 n)
+        bool enqueue(ElementType &newElement);
+
+        // Description: Removes (but does not return) the element with the next
+        //              "highest" priority value from the Priority Queue.
+        // Precondition: This Priority Queue is not empty.
+        // Exception: Throws EmptyDataCollectionException if Priority Queue is empty.
+        // Time Efficiency: O(log2 n)
+        void dequeue();
+   
+        // Description: Returns (but does not remove) the element with the next 
+        //              "highest" priority from the Priority Queue.
+        // Precondition: This Priority Queue is not empty.
+        // Postcondition: This Priority Queue is unchanged by this operation.
+        // Exception: Throws EmptyDataCollectionException if this Priority Queue is empty.
+        // Time Efficiency: O(1)
+        ElementType &peek() const;
+
+        // Description: Prints all elements in the priority queue for testing purposes.
+        // Time Efficiency: O(n)
+        // Precondition: This Priority Queue is not empty.
+        // Postcondition: This Priority Queue is unchanged by this operation.
+        // Exception: Throws EmptyDataCollectionException if Priority Queue is empty.
+        void printPriorityQueue() const;
+};
 #include "PriorityQueue.cpp"
+#endif
