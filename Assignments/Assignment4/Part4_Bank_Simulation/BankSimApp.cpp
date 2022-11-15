501018ece9b1b4113326c02a3b217b0e772acaae
Steven Wong <syw14@sfu.ca>
2022-11-14 17:30:58 -0800

    non-working copy

--- Assignments/Assignment4/Part4_Bank_Simulation/BankSimApp.cpp
+++ Assignments/Assignment4/Part4_Bank_Simulation/BankSimApp.cpp
@@ -7,34 +7,6 @@
  * Last Modification: November 2022
  */
 
-// Simulation Begins
-// Processing an arrival event at time:     1
-// Processing an arrival event at time:     2
-// Processing an arrival event at time:     4
-// Processing a departure event at time:    6
-// Processing a departure event at time:   11
-// Processing a departure event at time:   16
-// Processing an arrival event at time:    20
-// Processing an arrival event at time:    22
-// Processing an arrival event at time:    24
-// Processing a departure event at time:   25
-// Processing an arrival event at time:    26
-// Processing an arrival event at time:    28
-// Processing an arrival event at time:    30
-// Processing a departure event at time:   30
-// Processing a departure event at time:   35
-// Processing a departure event at time:   40
-// Processing a departure event at time:   45
-// Processing a departure event at time:   50
-// Processing an arrival event at time:    88
-// Processing a departure event at time:   91
-// Simulation Ends
-
-// Final Statistics:
-
-// 	Total number of people processed: 10
-// 	Average amount of time spent waiting: 5.6
-
 #include <iostream>
 #include "Event.h"
 #include "EmptyDataCollectionException.h"
@@ -45,7 +17,11 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int main(int argc, char** argv)
+// Function prototypes
+void processArrival(Event &arrival, PriorityQueue<Event> &eventQueue, Queue<Event> &tellerLine, bool &tellerAvailable);
+void processDeparture(Event &departure, PriorityQueue<Event> &eventQueue, Queue<Event> &tellerLine, bool &tellerAvailable, int &cumulativeTime);
+
+int main(int argc, char* argv[])
 {
     // Initialize variables
     Queue<Event> tellerLine;
@@ -55,7 +31,7 @@ int main(int argc, char** argv)
     int cumulativeTime = 0;
 
     cout << "Simulation Begins" << endl;
-    // Create arrival events with command line input
+    // Read in events with command line input
     while (!cin.eof())
     {
         // Read in the arrival time and transaction time
@@ -70,66 +46,17 @@ int main(int argc, char** argv)
         people++;
     }
 
-    // While there are still events to be processed...
+    // While there remains events to be processed in the event queue...
     while (!eventQueue.isEmpty())
     {
-        // Get the next event from the eventQueue and dequeue it
+        // Get the next highest priority event and get current time
         Event nextEvent = eventQueue.peek();
-        eventQueue.dequeue();
 
-        // If the next event is an arrival
-        if (nextEvent.getType() == 'A')
-        {
-            // If the teller is available
-            if (tellerAvail)
-            {
-                // Process the arrival event
-                cout << "Processing an arrival event at time: " << nextEvent.getTime() << endl;
-                // Set the teller to unavailable
-                tellerAvail = false;
-                // Create a departure event
-                Event departureEvent('D', nextEvent.getTime() + nextEvent.getLength(), nextEvent.getLength());
-                // Enqueue the departure event
-                eventQueue.enqueue(departureEvent);
-            }
-            // If the teller is not available
-            else
-            {
-                // Process the arrival event
-                cout << "Processing an arrival event at time: " << nextEvent.getTime() << endl;
-                // Enqueue the arrival event
-                tellerLine.enqueue(nextEvent);
-            }
-        }
-
-        // If the next event is a departure
-        else if (nextEvent.getType() == 'D')
-        {
-            // Process the departure event
-            cout << "Processing a departure event at time: " << nextEvent.getTime() << endl;
-            // Calculate the time spent waiting
-            int timeSpentWaiting = nextEvent.getTime() - nextEvent.getLength();
-            // Add the time spent waiting to the cumulative time
-            cumulativeTime += timeSpentWaiting;
-            // If the teller line is not empty
-            if (!tellerLine.isEmpty())
-            {
-                // Get the next arrival event from the teller line
-                Event nextArrivalEvent = tellerLine.peek();
-                // Dequeue the next arrival event
-                tellerLine.dequeue();
-                // Create a departure event
-                Event departureEvent('D', nextEvent.getTime() + nextArrivalEvent.getLength(), nextArrivalEvent.getLength());
-                // Enqueue the departure event
-                eventQueue.enqueue(departureEvent);
-            }
-            // If the teller line is empty
-            else
-            {
-                // Set the teller to available
-                tellerAvail = true;
-            }
-        }
+        // Process the event
+        if (nextEvent.isArrival())
+            processArrival(nextEvent, eventQueue, tellerLine, tellerAvail);
+        else
+            processDeparture(nextEvent, eventQueue, tellerLine, tellerAvail, cumulativeTime);
     }
 
     cout << "Simulation Ends" << endl << endl;
@@ -137,4 +64,50 @@ int main(int argc, char** argv)
     cout << "\tTotal number of people processed: " << people << endl;
     cout << "\tAverage amount of time spent waiting: " << (float)cumulativeTime / people << endl << endl;
     return 0;
+}
+
+// Process an arrival event
+void processArrival(Event &arrival, PriorityQueue<Event> &eventQueue, Queue<Event> &tellerLine, bool &tellerAvail)
+{
+    cout << "Processing an arrival event at time: " << arrival.getTime() << endl;
+    // Dequeue the arrival event
+    eventQueue.dequeue();
+
+    // If the teller is available, create a departure event and enqueue it
+    if (tellerLine.isEmpty() && tellerAvail)
+    {
+        Event departureEvent('D', arrival.getTime() + arrival.getLength(), 0);
+        eventQueue.enqueue(departureEvent);
+        tellerAvail = false;
+    }
+    else
+    {
+        tellerLine.enqueue(arrival);
+    }
+}
+
+// Process a departure event
+void processDeparture(Event &departure, PriorityQueue<Event> &eventQueue, Queue<Event> &tellerLine, bool &tellerAvail, int &cumulativeTime)
+{
+    cout << "Processing a departure event at time: " << departure.getTime() << endl;
+    // Dequeue the departure event
+    eventQueue.dequeue();
+
+    // If the teller line is not empty, create a departure event and enqueue it
+    if (!tellerLine.isEmpty())
+    {
+        // Customer at front of line begins transaction
+        Event customer = tellerLine.peek();
+        cout << "Values going in: " << departure.getTime() << " " << customer.getTime() << endl;
+        cumulativeTime += (departure.getTime() - customer.getTime());
+        cout << "Cumulative time spent waiting: " << cumulativeTime << endl;
+
+        tellerLine.dequeue();
+        Event nextDeparture('D', departure.getTime() + customer.getLength());
+        eventQueue.enqueue(nextDeparture);
+    }
+    else
+    {
+        tellerAvail = true;
+    }
 }
