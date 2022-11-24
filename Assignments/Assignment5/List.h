ff7e79465bc2e4ccab3cfad91cb41f5ab22818aa
Steven Wong <syw14@sfu.ca>
2022-11-24 01:19:45 -0800

    rm trailing whitespace

--- Assignments/Assignment5/List.h
+++ Assignments/Assignment5/List.h
@@ -1,9 +1,9 @@
 /*
  * List.h
- * 
+ *
  * Class Description: List data collection ADT.
- *                    Based on the Hashing strategy and the open addressing 
- *                    collision resolution strategy called linear probing hashing.             
+ *                    Based on the Hashing strategy and the open addressing
+ *                    collision resolution strategy called linear probing hashing.
  * Class Invariant: Data collection with the following characteristics:
  *                  - Each element is unique (no duplicates).
  *
@@ -24,28 +24,28 @@ class List  {
 
 private:
 
-/* 
+/*
  * For experimentation purposes, you can add private data members to this List class.
  */
 
-  Member * * hashTable = nullptr;           // HashTable - underlying data structure (array) of our Data Collection.
+  Member **hashTable = nullptr;           // HashTable - underlying data structure (array) of our Data Collection.
                                             // HashTable is a pointer to an array of pointers to objects of Member class
   unsigned int elementCount = 0;            // Current number of elements stored into Data Collection.
   unsigned int (*hashFcn)(string name);     // Pointer to hash function.
 
 public:
 
-/* 
+/*
  * You can add more private methods to this class, but you cannot remove the public methods below nor can you change their prototype.
  * For experimentation purposes, you can add public methods to this List class.
- * 
+ *
  */
 
   constexpr static unsigned int CAPACITY = 100;  // Size of hashTable - underlying data structure (array) of List.
-  
+
   // Constructor
   List(unsigned int (*hFcn)(string));
-  
+
   // Destructor
   // Description: Destruct a List object, releasing heap-allocated memory.
   ~List();
@@ -55,23 +55,23 @@ public:
   unsigned int getElementCount() const;
 
   // Description: Insert an element.
-  // NOTE: You do not have to expand the hashTable when it is full. 
-  // Precondition: newElement must not already be in in the List.  
-  // Postcondition: newElement inserted and elementCount has been incremented. 
+  // NOTE: You do not have to expand the hashTable when it is full.
+  // Precondition: newElement must not already be in in the List.
+  // Postcondition: newElement inserted and elementCount has been incremented.
   // Exception: Throws UnableToInsertException if we cannot insert newElement in the List.
   //            For example, if the operator "new" fails, or hashTable is full (temporary solution).
-  // Exception: Throws ElementAlreadyExistsException if newElement is already in the List.  
-  void insert( Member & newElement );
-   
+  // Exception: Throws ElementAlreadyExistsException if newElement is already in the List.
+  void insert(Member &newElement);
+
   // Description: Returns a pointer to the target element if found.
   // Postcondition: List remains unchanged.
   // Exception: Throws EmptyDataCollectionException if the List is empty.
   // Exception: Throws ElementDoesNotExistException if newElement is not found in the List.
-  Member * search( Member & target ) const;
-  
+  Member *search(Member &target) const;
+
   // Description: Prints all elements stored in the List (unsorted).
   // Postcondition: List remains unchanged.
   void printList() const;
 
 }; // end List.h
-#endif  
+#endif
