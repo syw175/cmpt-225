3555750e103fca8202434ec6db636b769f1ac757
Steven Wong <syw14@sfu.ca>
2022-11-30 21:47:17 -0800

    complete number reverser and 3 hash funcs

--- Assignments/Assignment5/hashFunctionTestDriver.cpp
+++ Assignments/Assignment5/hashFunctionTestDriver.cpp
@@ -25,44 +25,89 @@
 // If we do not have enough space to report our results as comments above each hash function, we can add them as comments at the bottom of our file.
 // Finally, conclude which hash function best spreads over the whole hash table the hash table indices it produces.
 
-// Time efficiency: ...
-// Space efficiency: ...
+// Time efficiency: O(1)
+// Space efficiency: O(1)
 // Hash Function 1: A simple modulo hashing method that takes the phone number and divides it by the capacity M; taking
-//                  the remainder as our hash index. 
+//                  the remainder as our hash index through the modulo operator.
 unsigned int hashSimpleModulo(string phone);
 
-// Time efficiency: ...
-// Space efficiency: ...
-// Hash Function 2: Breaks up components of phone into XXX-YYY-ZZZZ, reversing Xs and Zs. Take the sum the components
+// Time efficiency: O(1)
+// Space efficiency: O(1)
+// Hash Function 2: Sum of the 1-3th digits, 4-6th digits, and 7-10th digits of the phone number modulo M           
+unsigned int hashSimpleShift(string phone);
+
+// Time efficiency: O(1)
+// Space efficiency: O(1)
+// Hash Function 3: Breaks up components of phone into XXX-YYY-ZZZZ, reversing Xs and Ys. Take the sum the components
 //                  and divide by capacity M; taking the remainder as our hash index.
 unsigned int hashBoundaryFolding(string phone);
 
-// Time efficiency: ...
-// Space efficiency: ...
-// Hash Function 3:
-unsigned int hashToBeNamed(string phone);
-
 // Create a number of indexing keys (phone numbers)
 void createRandomTestData(unsigned int size, unsigned int digitCount);
+
+// Reverse the digits of a given number
+unsigned int reverseDigits(unsigned int number);
 
 // Start program
 int main(int argc, char *argv[])
 {
     // Initialize variables
     List *testHashFunc1 = new List(hashSimpleModulo);
-    List *testHashFunc2 = new List(hashBoundaryFolding);
-    List *testHashFunc3 = new List(hashToBeNamed);
+    List *testHashFunc2 = new List(hashSimpleShift);
+    List *testHashFunc3 = new List(hashBoundaryFolding);
     string aPhoneNumber = "";
 
-    // Read input from stdin
-    if ()
-
-
-
-
-
-
-
-
+    // INCOMPLETE
     return 0;
+}
+
+// A simple modular arithmetic hashing method
+unsigned int hashSimpleModulo(string phone)
+{
+    unsigned int hashcode = stoul(phone);
+    return hashcode % List::CAPACITY;
+}
+
+// A simple shift hashing method
+unsigned int hashSimpleShift(string phone)
+{
+    unsigned int firstThree = stoul(phone.substr(0, 3));
+    unsigned int secondThree = stoul(phone.substr(3, 3));
+    unsigned int lastFour = stoul(phone.substr(6, 4));
+    // Add all three components and modulo by capacity to get hashcode
+    unsigned int hashcode = (firstThree + secondThree + lastFour) % List::CAPACITY;
+}
+
+// A simple boundary folding hashing method
+unsigned int hashBoundaryFolding(string phone)
+{
+    unsigned int firstThree = stoul(phone.substr(0, 3));
+    unsigned int secondThree = stoul(phone.substr(3, 3));
+    unsigned int lastFour = stoul(phone.substr(6, 4));
+
+    // Reverse first three digits and second three digits
+    unsigned int reversedFirstThree = reverseDigits(firstThree);
+    unsigned int reversedSecondThree = reverseDigits(secondThree);
+
+    // Sum all three components and modulo by capacity to get hashcode
+    return (reversedFirstThree + reversedSecondThree + lastFour) % List::CAPACITY;
+}
+
+// Simple function to reverse the digits of a number
+unsigned int reverseDigits(unsigned int number)
+{
+    int reversed = 0;
+    // While number is not 0, keep dividing by 10 to get the last digit 
+    while (number > 0)
+    {
+        reversed = reversed * 10 + number % 10;
+        number /= 10;
+    }
+    return reversed;
+}
+
+// Create a number of indexing keys (phone numbers)
+void createRandomTestData(unsigned int size, unsigned int digitCount)
+{
+    // Incomplete
 }
